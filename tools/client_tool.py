7ab3dcee686cbc04324a587bbc344ce807d49b1e
Enrique Garcia Illera <egaillera@gmail.com>
2024-03-14 18:45:33 +0100

    [feat] Change return value of this tool

--- tools/client_tool.py
+++ tools/client_tool.py
@@ -3,4 +3,4 @@ from langchain.agents import tool
 @tool
 def get_client_info(client_id:str) -> str:
     """ Returns relevant information about a client"""
-    return "El cliente solo ha invertido en letras del tesoro y es averso al riesgo"
+    return "El cliente tiene baja tolerancia al riesgo y le gustan los productos de BBVA"
