ee3b4f12b165ca9841e2c8d1e1e86ea86a093758
Enrique Garcia Illera <egaillera@gmail.com>
2024-03-26 10:39:58 +0100

    [feat] Version of SQL agent with few shot prompt

--- agents/clients_agent.py
+++ agents/clients_agent.py
@@ -1,3 +1,19 @@
+from langchain_community.vectorstores import FAISS
+from langchain_core.example_selectors import SemanticSimilarityExampleSelector
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_community.utilities import SQLDatabase
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+from langchain_core.prompts import (
+    ChatPromptTemplate,
+    FewShotPromptTemplate,
+    MessagesPlaceholder,
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+)
+
 examples = [
     {"input": "Listame todos los clientes.", "query": "SELECT * FROM clientes;"},
     {
@@ -42,72 +58,64 @@ examples = [
     },
 ]
 
-from langchain_community.vectorstores import FAISS
-from langchain_core.example_selectors import SemanticSimilarityExampleSelector
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.agent_toolkits import create_sql_agent
-from langchain_community.utilities import SQLDatabase
-from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
 
-example_selector = SemanticSimilarityExampleSelector.from_examples(
-    examples,
-    OpenAIEmbeddings(),
-    FAISS,
-    k=5,
-    input_keys=["input"],
-)
+def create_clients_agent():
 
-from langchain_core.prompts import (
-    ChatPromptTemplate,
-    FewShotPromptTemplate,
-    MessagesPlaceholder,
-    PromptTemplate,
-    SystemMessagePromptTemplate,
-)
+    example_selector = SemanticSimilarityExampleSelector.from_examples(
+        examples,
+        OpenAIEmbeddings(),
+        FAISS,
+        k=5,
+        input_keys=["input"],
+    )
 
-system_prefix = """You are an agent designed to interact with a SQL database that contains information about clients of a bank.
-Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
-Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
-You can order the results by a relevant column to return the most interesting examples in the database.
-Never query for all the columns from a specific table, only ask for the relevant columns given the question.
-You have access to tools for interacting with the database.
-Only use the given tools. Only use the information returned by the tools to construct your final answer.
-You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+    system_prefix = """You are an agent designed to interact with a SQL database that contains information about clients of a bank.
+    Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
+    Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
+    You can order the results by a relevant column to return the most interesting examples in the database.
+    Never query for all the columns from a specific table, only ask for the relevant columns given the question.
+    You have access to tools for interacting with the database.
+    Only use the given tools. Only use the information returned by the tools to construct your final answer.
+    You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
 
-DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+    DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
 
-If the question does not seem related to the database, just return "I don't know" as the answer.
+    If the question does not seem related to the database, just return "I don't know" as the answer.
 
-Here are some examples of user inputs and their corresponding SQL queries:"""
+    Here are some examples of user inputs and their corresponding SQL queries:"""
 
-few_shot_prompt = FewShotPromptTemplate(
-    example_selector=example_selector,
-    example_prompt=PromptTemplate.from_template(
-        "User input: {input}\nSQL query: {query}"
-    ),
-    input_variables=["input", "dialect", "top_k"],
-    prefix=system_prefix,
-    suffix="",
-)
+    few_shot_prompt = FewShotPromptTemplate(
+        example_selector=example_selector,
+        example_prompt=PromptTemplate.from_template(
+            "User input: {input}\nSQL query: {query}"
+        ),
+        input_variables=["input", "dialect", "top_k"],
+        prefix=system_prefix,
+        suffix="",
+    )
 
-full_prompt = ChatPromptTemplate.from_messages(
-    [
-        SystemMessagePromptTemplate(prompt=few_shot_prompt),
-        ("human", "{input}"),
-        MessagesPlaceholder("agent_scratchpad"),
-    ]
-)
+    full_prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessagePromptTemplate(prompt=few_shot_prompt),
+            ("human", "{input}"),
+            MessagesPlaceholder("agent_scratchpad"),
+        ]
+    )
 
-db = SQLDatabase.from_uri("sqlite:///dbclientes.db")
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-agent = create_sql_agent(
-    llm=llm,
-    db=db,
-    prompt=full_prompt,
-    verbose=True,
-    agent_type="openai-tools",
-)
+    db = SQLDatabase.from_uri("sqlite:///dbclientes.db")
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+    agent = create_sql_agent(
+        llm=llm,
+        db=db,
+        prompt=full_prompt,
+        verbose=True,
+        agent_type="openai-tools",
+    )
 
-agent.invoke({"input": "Que clientes con nivel adquisitivo alto son menores de 30 años y viven en España"})
+    return agent
+
+if __name__ == "__main__":
+    
+    client_agent = create_clients_agent()
+    client_agent.invoke({"input": "Dame 20 clentes con nivel adquisitivo alto, que sean menores de 20 años y que sean residentes en España"})
 
